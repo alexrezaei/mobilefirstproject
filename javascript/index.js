829a268bad9987bb843b2172a9e73aa985beb706
alexrezaei <74854699+alexrezaei@users.noreply.github.com>
2021-05-17 12:39:56 +0200

    changes to contact form

--- javascript/index.js
+++
@@ -1,8 +0,0 @@
-const toggleButton = document.getElementById("toggleButton")
-const ulList = document.getElementById("navList")
-
-
-toggleButton.addEventListener("click", () => {
-    console.log("fuckery")
-    ulList.classList.toggle('active');
-})
